ec7b316ff3f70a98c5b5bfc11c4d34708fab385b
kashishptl <kashishpatel0828@gmail.com>
2026-05-06 10:23:46 +0530

    image

--- config/urls.py
+++ config/urls.py
@@ -20,6 +20,8 @@ from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -40,4 +42,4 @@ urlpatterns = [
     path('api/categories/', include('apps.categories.urls')),
     path('api/cart/', include('apps.cart.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0)),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
